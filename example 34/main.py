88240d110b1cb22ee1b5ccaaa40433ad08a30056
ruiwu1990 <rui@cse.unr.edu>
2019-03-12 15:57:49 -0400

    update

--- example 34/main.py
+++ example 34/main.py
@@ -1,19 +1,30 @@
 from flask import Flask, render_template
 import util
 
-
+# create an application instance
+# all requests it receives from clients to this object for handling
+# 
 app = Flask(__name__)
 
-
+# route is used to map a URL with a Python function
 @app.route('/')
+# this is how you define a function in Python
 def index():
     # this is your index page
+    # no need to specify which type this varible is...
+    # in Python you can even do this:
+    # a = 'aaa'
+    # a = 1
     log = 'Hello world.'
+    # using render_template function, Flask will search
+    # the file named index.html under templates folder
     return render_template('index.html', log_html = log)
 
 
 if __name__ == '__main__':
+	# set debug mode
     app.debug = True
+    # your local machine ip
     ip = '127.0.0.1'
     app.run(host=ip)
 
---
+++ example 37/main.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, jsonify, json
+import util
+
+
+app = Flask(__name__)
+
+@app.route('/api/random_number', methods=['GET'])
+def api_random_num():
+	'''
+	RESTful API: generate a random number in a JSON file
+	'''
+	rand_num = util.random_int()
+	tmp_name = 'random_number'
+	json_dict = {
+		'name': tmp_name,
+		'number': rand_num
+	}
+
+	# convert dictionary to json obj
+	json_obj = json.dumps(json_dict)
+	return json_obj
+
+
+@app.route('/')
+def index():
+    # this is your index page
+    log = 'Hello world.'
+    return render_template('index.html', log_html = log)
+
+
+if __name__ == '__main__':
+    app.debug = True
+    ip = '127.0.0.1'
+    app.run(host=ip)
+
