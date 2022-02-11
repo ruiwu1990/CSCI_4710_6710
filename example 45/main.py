d7f54b08b4276e87bc66f22419fcb80a8afbdce5
ruiwu1990 <raywu1990@nevada.unr.edu>
2022-02-11 11:18:30 -0500

    post data

---
+++ example 45/main.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template,request, jsonify
+import util
+
+# create an application instance
+# all requests it receives from clients to this object for handling
+# we are instantiating a Flask object by passing __name__ argument to the Flask constructor. 
+# The Flask constructor has one required argument which is the name of the application package. 
+# Most of the time __name__ is the correct value. The name of the application package is used 
+# by Flask to find static assets, templates and so on.
+app = Flask(__name__)
+
+# route is used to map a URL with a Python function
+# complete address: ip:port/
+# 127.0.0.1:5000/
+@app.route('/')
+# this is how you define a function in Python
+def index():
+    # this is your index page
+    # no need to specify which type this varible is...
+    # in Python you can even do this:
+    # a = 'aaa'
+    # a = 1
+    log = 'Hello world.'
+    # using render_template function, Flask will search
+    # the file named index.html under templates folder
+    return render_template('index.html', log_html = log)
+
+@app.route('/send_data', methods=['POST'])
+def receive_data():
+
+    request_data = request.get_json()
+
+    data = request_data['send_data']
+
+    print(data)
+
+    # DO SOME WORK ...
+
+    toReturn = {"return": "True"}
+
+    return jsonify(toReturn)
+
+
+@app.route('/receivedata', methods=['POST'])
+def receive_data2():
+    print(request.form['myData'])
+    return 'data received'
+
+if __name__ == '__main__':
+	# set debug mode
+    app.debug = True
+    # your local machine ip
+    ip = '127.0.0.1'
+    app.run(host=ip)
+
